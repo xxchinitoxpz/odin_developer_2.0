6575168a0647dcb7b58a7c0056c6e9590d4bbf72
xxchinitoxpz <brayanhornamartinez@gmail.com>
2024-06-18 20:47:15 -0500

    second commit

--- resources/views/project/projectpage.blade.php
+++ resources/views/project/projectpage.blade.php
@@ -17,7 +17,18 @@
 
     <!-- Header -->
     @include('page.header')
-
+    <div class="container-fluid page-header py-5 mb-5" bis_skin_checked="1">
+        <div class="container py-5" bis_skin_checked="1">
+            <h1 class="display-3 text-white mb-3 animated slideInDown">Nuestros proyectos</h1>
+            <nav aria-label="breadcrumb animated slideInDown">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
+                    <li class="breadcrumb-item text-white active" aria-current="page">Projects</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
     <!-- Header -->
     @include('project.projects')
 
